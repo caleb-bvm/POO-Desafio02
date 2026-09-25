f00301a370ff4aa1d1a2f1b4e84b934560cd2358
caleb-bvm <calebortiz288@gmail.com>
2026-09-25 13:06:23 -0600

    1st Commit

---
+++ src/main/java/com/mycompany/desafio02/Desafio02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.desafio02;
+
+/**
+ *
+ * @author caleb
+ */
+public class Desafio02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
